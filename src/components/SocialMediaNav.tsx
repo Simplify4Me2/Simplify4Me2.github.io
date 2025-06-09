b08e22d151e36d03c730b8a68c727dfbc6093634
Simon <simon.vereecke@live.com>
2025-06-09 22:13:26 +0200

    feat: Wrap social media icons in anchor tags for improved navigation

--- src/components/SocialMediaNav.tsx
+++ src/components/SocialMediaNav.tsx
@@ -13,13 +13,13 @@ export function SocialMediaNav({ inverted = false, className }: SocialMediaNavPr
     return <nav>
     <ul className={`flex gap-4 lg:items-center ${className}`}>
       <li>
-        <img src={GitHubIcon} className={iconClass} />
+        <a href="https://github.com/Simplify4Me2"><img src={GitHubIcon} className={iconClass} /></a>
       </li>
       <li>
-        <img src={XIcon} className={iconClass} />
+        <a href="https://x.com/simon_vereecke"><img src={XIcon} className={iconClass} /></a>
       </li>
       <li>
-        <img src={LinkedInIcon} className={iconClass} />
+        <a href="https://www.linkedin.com/in/simon-vereecke-75822497/"><img src={LinkedInIcon} className={iconClass} /></a>
       </li>
     </ul>
   </nav>
